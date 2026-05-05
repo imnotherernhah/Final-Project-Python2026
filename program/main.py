eb268f9d817e7a533a8a995c107b2b0f339e063b
imnotherernhah <zjpared61@hatboro-horsham.org>
2026-05-05 12:00:04 -0400

    Files can be chosen upon opening

--- program/main.py
+++ program/main.py
@@ -3,6 +3,7 @@ import pygame
 import io
 import customtkinter as tk
 from customtkinter import CTkImage
+from tkinter import filedialog
 from PIL import Image
 import time
 from pathlib import Path
@@ -14,21 +15,19 @@ root.geometry("500x500")
 pygame.mixer.pre_init(44100, -16, 2, 2048)
 pygame.mixer.init()
 pygame.init()
-mus=pygame.mixer.music.load("R1B.ogg")
-pygame.mixer.music.play()
 def updProgBar(progbar,tag, elapsed, tota):
     tottime=tag.duration
     #print(round(tottime/60,2))
     secs=(pygame.mixer_music.get_pos())/1000
     progbar.set(secs/tottime)
     elapsed.configure(text=timedelta(seconds=math.trunc(secs)))
-    tota.configure(text=timedelta(minutes=round(tottime)))
+    tota.configure(text=timedelta(seconds=math.trunc(tottime)))
     root.update()
 
 def MainProg2():
-    tag = TinyTag.get('R1B.ogg', image=True)
+    tag = TinyTag.get(pathtosng, image=True)
     if tag.title is None:
-        songtit=tag.filename
+        songtit=name
     else:
         songtit=tag.title
     if tag.artist is None:
@@ -39,15 +38,15 @@ def MainProg2():
         album="Unknown Album"
     else:
         album=tag.album
-    songname = tk.CTkLabel(root, text=songtit, font=("Arial", 22))
-    songname.place(relx=0.5, rely=0.65, anchor='c')
+    songname = tk.CTkLabel(root, text=songtit, font=("Arial", 20))
+    songname.place(relx=0.5, rely=0.665, anchor='c')
     songart = tk.CTkLabel(root, text=songartist, font=("Arial", 15))
-    songart.place(relx=0.5, rely=0.7, anchor='c')
-    songal = tk.CTkLabel(root, text=album, font=("Arial", 20))
-    songal.place(relx=0.5, rely=0.75, anchor='c')
+    songart.place(relx=0.5, rely=0.715, anchor='c')
+    songal = tk.CTkLabel(root, text=album, font=("Arial", 17))
+    songal.place(relx=0.5, rely=0.76, anchor='c')
     cover = tk.CTkImage(light_image=Image.open("cover.png"), dark_image=Image.open("cover.png"), size=(300, 300))
     label = tk.CTkLabel(root, image=cover, text="")
-    label.place(relx=0.5, rely=0.3, anchor='c')
+    label.place(relx=0.5, rely=0.34, anchor='c')
     progressbar = tk.CTkProgressBar(root, orientation="horizontal",progress_color="purple")
     progressbar.place(relx=0.5, rely=0.81, anchor='c')
     elap = tk.CTkLabel(root, text="", font=("Arial", 17))
@@ -58,10 +57,10 @@ def MainProg2():
         updProgBar(progressbar, tag, elap,tot)
 
 def MainProg():
-    file_path = "R1B.ogg"
+    file_path = pathtosng
     filename = Path(file_path).stem
     print(filename)
-    tag = TinyTag.get('R1B.ogg', image=True)
+    tag = TinyTag.get(pathtosng, image=True)
     songtit=tag.title
     songartist=tag.artist
     album=tag.album
@@ -74,6 +73,16 @@ def MainProg():
         img2 = Image.open(io.BytesIO(imgdat))
         img2.save('cover.png')
     MainProg2()
+pathtosng = filedialog.askopenfilename(
+    initialdir="/",
+    title="Choose an audio file to play.",
+    filetypes=(("Supported audio files (.ogg, .wav, .mp3, .flac)", "*.ogg; *.wav; *.mp3; *.flac"), ("All files", "*.*"))
+)
+pat=Path(pathtosng)
+name=pat.name
 
+print(f"Selected file: {pathtosng}")
+mus=pygame.mixer.music.load(pathtosng)
+pygame.mixer.music.play()
 MainProg()
 #root.mainloop()
